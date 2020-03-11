cee950a37697935a45f1d77256f667fdedb157c6
Diego De la Vega <diego_dv7@outlook.com>
2020-03-10 18:19:51 -0600

    app6 changes

--- RealTime Marker/index.js
+++ RealTime Marker/index.js
@@ -16,19 +16,23 @@ function startMap()
     var icono = {
         url : "https://media1.giphy.com/media/CXG1VWTkjEgHC/source.gif",
         scaledSize: new google.maps.Size(50, 50),
-        origin:  new google.maps.Point(0, 0),
-        anchor : new google.maps.Point(0, 0)
+        origin:  new google.maps.Point(0,0),
+        anchor : new google.maps.Point(0,0)
     };
 
     var marker = new google.maps.Marker({
-        position: {lat: 0, long: 0},
+        position: {lat: 0, lng: 0},
+        icon: icono,
         map : map
     });
 
 
     if(navigator.geolocation)
     {
-        movePosition(marker);
+        setInterval(function() {
+            movePosition(marker);
+        },3000);
+        
     }
 
 }
@@ -38,14 +42,13 @@ function movePosition(marker){
     navigator.geolocation.getCurrentPosition(position => {
 
         var pos = {
-            lat : position.coords.latitude,
-            lng : position.coords.longitude
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
         }
 
 
-        marker.setPosition(new google.maps.LatLng(position.coordinates.lat, position.coordinates.lng));
-
-        map.panTo(new google.maps.LatLng(position.coordinates.lat, position.coordinates.lng));
+        marker.setPosition(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+        map.panTo(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
         map.setCenter(pos);
     });
 
