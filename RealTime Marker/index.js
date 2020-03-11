8c073e3a2258c16b1e595a6dbdbf6e41f88a13e6
Diego De la Vega <diego_dv7@outlook.com>
2020-03-10 18:07:45 -0600

    app6 correction

--- RealTime Marker/index.js
+++ RealTime Marker/index.js
@@ -15,15 +15,13 @@ function startMap()
     map = new google.maps.Map(document.getElementById("map"), properties);
     var icono = {
         url : "https://media1.giphy.com/media/CXG1VWTkjEgHC/source.gif",
-        scaledSize: new google.maps.Size(50,50),
-        origin:  new google.maps.Point(0,0),
-        anchor : new google.maps.Point(0,0)
+        scaledSize: new google.maps.Size(50, 50),
+        origin:  new google.maps.Point(0, 0),
+        anchor : new google.maps.Point(0, 0)
     };
-    
+
     var marker = new google.maps.Marker({
         position: {lat: 0, long: 0},
-        icon: icono,
-        scaledSize : new google.maps.Size(50,50),
         map : map
     });
 
