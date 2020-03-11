2702db6504f60cc7652d1d2c774712c33e031695
Diego De la Vega <diego_dv7@outlook.com>
2020-03-10 18:01:26 -0600

    app6

---
+++ RealTime Marker/index.js
@@ -0,0 +1,51 @@
+var coordinates = {
+    lat: 0,
+    long: 0
+}
+
+var properties = {
+    center: coordinates,
+    zoom: 20
+}
+
+
+function startMap()
+{
+
+    map = new google.maps.Map(document.getElementById("map"), properties);
+    var icono = {
+        url : "https://media1.giphy.com/media/CXG1VWTkjEgHC/source.gif"
+    };
+    var marker = new google.maps.Marker({
+        position: {lat: 0, long: 0},
+        icon: icono,
+        scaledSize : new google.maps.Size(50,50),
+        map : map
+    });
+
+
+    if(navigator.geolocation)
+    {
+        movePosition(marker);
+    }
+
+}
+
+function movePosition(marker){
+
+    navigator.geolocation.getCurrentPosition(position => {
+
+        var pos = {
+            lat : position.coords.latitude,
+            lng : position.coords.longitude
+        }
+
+
+        marker.setPosition(new google.maps.LatLng(position.coordinates.lat, position.coordinates.lng));
+
+        map.panTo(new google.maps.LatLng(position.coordinates.lat, position.coordinates.lng));
+        map.setCenter(pos);
+    });
+
+
+}
