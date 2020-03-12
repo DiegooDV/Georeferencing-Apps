0c7337dbabc78a2f666f51926da9f52b0e5298ed
Diego De la Vega <diego_dv7@outlook.com>
2020-03-12 00:58:55 -0600

    gif

--- RealTime Marker/index.js
+++ RealTime Marker/index.js
@@ -14,7 +14,7 @@ function startMap()
 
     map = new google.maps.Map(document.getElementById("map"), properties);
     var icono = {
-        url : "https://lh3.googleusercontent.com/proxy/yjn-vm0-BIBmIhAaEeHmA4cF72PUvkmGVBEDc9j9VRpzvYzucuztygOyjX8j2fgumx1ZIuVsnzELBpFqGo4GTnZp8OMHskErb0JNtQsPEHmH0MHft7iSlhztQcY",
+        url : "https://lh3.googleusercontent.com/proxy/0IOJ-i9lDa_ZvN32g8Dau9HyMzbVOaC4Qb-CIKHhBdQBsW8aNzaGrjxUvMmN8jyyXKq1qjYjLFq5yhVtlr_Q76JwCHE7fKv72dDzUz2Ipxeyy9GF83xkjvd6UMM",
         scaledSize: new google.maps.Size(50, 50),
         origin:  new google.maps.Point(0, 0),
         anchor : new google.maps.Point(0, 0)
