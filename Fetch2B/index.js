3a27906fbc17b8d43c06c9bed0ac0e94d442dd53
Diego De la Vega <diego_dv7@outlook.com>
2020-03-23 17:53:09 -0600

    Fetch 2 style

--- Fetch2B/index.js
+++ Fetch2B/index.js
@@ -7,16 +7,16 @@ fetch('https://corona.lmao.ninja/countries')
 
        data.forEach(element => {
            console.log(element.country);
-           let row = document.createElement("div");
-           row.className = "row border border-dark";
-           countries.appendChild(row);
+           //let row = document.createElement("div");
+         //  row.className = "row border border-dark";
+          // countries.appendChild(row);
 
            let column = document.createElement("div");
-           row.className = "col-12";
-           row.appendChild(column);
+           column.className = "col-4 border border-danger";
+           countries.appendChild(column);
 
            let name = document.createElement("p");
-           name.textContent = `Country: ${element.country}, cases: ${element.cases}`
+           name.innerHTML = `<strong>Country:</strong> ${element.country} <br> <strong>Cases: </strong> ${element.cases}`
            column.appendChild(name);
        });
 
