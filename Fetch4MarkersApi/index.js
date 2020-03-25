cf5958a56ac415d68888cc0037a4757a26852944
Diego De la Vega <diego_dv7@outlook.com>
2020-03-24 18:49:05 -0600

    fetch4

---
+++ Fetch4MarkersApi/index.js
@@ -0,0 +1,25 @@
+var coordinates = {
+    lat: 0,
+    lng: 0
+}
+
+var properties = {
+    center: coordinates,
+    zoom: 2
+}
+
+
+function mapStart()
+{
+   fetch("https://corona.lmao.ninja/countries")
+   .then(function(response) {
+
+    response.json.then(function(data){
+        console.log(data);
+    })
+
+   })
+   .catch(function(error) {
+       console.log(error.message);
+   });
+}
