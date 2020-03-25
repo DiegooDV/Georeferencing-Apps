af716558fdb66d110398b656710c3d189d6ee048
Diego De la Vega <diego_dv7@outlook.com>
2020-03-24 19:42:35 -0600

    fetch4

--- Fetch4MarkersApi/index.js
+++ Fetch4MarkersApi/index.js
@@ -20,17 +20,28 @@ function mapStart()
         console.log(data[0]);
         data.forEach(element => {
 
-            var info = `<img src="${datacountryInfo.flag}">`
+            var info = `<img src="${element.countryInfo.flag}">`
 
             var infowindow = new google.maps.InfoWindow({
                 content: info
             })
+
+            if(element.country.toLowerCase() == "usa" )
+            {
+                let marker = new google.maps.Marker({
+                    map: map,
+                    position: new google.maps.LatLng(37.09024, -95.712891),
+                    title: 'United States'
+                });
+            }
+            else{
 
                 let marker = new google.maps.Marker({
                     map: map,
                     position: new google.maps.LatLng(element.countryInfo.lat, element.countryInfo.long),
                     title: element.country
                 });
+            }
 
                 marker.addListener('click', function() {
                   infowindow.open(map, marker);
