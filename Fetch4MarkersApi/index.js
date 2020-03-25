008b2524a38049308cfdb09272e9d3fa1b24d7b7
Diego De la Vega <diego_dv7@outlook.com>
2020-03-24 20:22:56 -0600

    fetch4

--- Fetch4MarkersApi/index.js
+++ Fetch4MarkersApi/index.js
@@ -22,13 +22,13 @@ function mapStart()
 
             var icon = {
                 url : "../Icons/corona.png",
-                scaledSize: new google.maps.Size(20, 20),
+                scaledSize: new google.maps.Size(70, 70),
                 origin:  new google.maps.Point(0, 0),
                 anchor : new google.maps.Point(0, 0)
             };
 
-            var info = `<img src="${element.countryInfo.flag}">
-            <br><Strong>Cases:</Strong> ${element.cases} 
+            var info = `<img class="responsiveImg" src="${element.countryInfo.flag}">
+               <br><Strong>Cases:</Strong> ${element.cases} 
                <br><Strong>Deaths:</Strong> <span class="text-danger">${element.deaths}</span>
                <br><Strong>Today cases:</Strong> ${element.todayCases}
                <br><Strong>Today deaths:</Strong> <span class="text-danger">${element.todayDeaths}</span>`
@@ -45,12 +45,14 @@ function mapStart()
                 marker.title = "United States";
                 marker.position =  new google.maps.LatLng(37.09024, -95.712891);
 
-               info = `<img class="responsiveImg"
+               info = `<div class="gm-style-iw">
+               <img class="responsiveImg"
                src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png">
                <br><Strong>Cases:</Strong> ${element.cases} 
                <br><Strong>Deaths:</Strong> <span class="text-danger">${element.deaths}</span>
                <br><Strong>Today cases:</Strong> ${element.todayCases}
-               <br><Strong>Today deaths:</Strong> <span class="text-danger">${element.todayDeaths}</span>`
+               <br><Strong>Today deaths:</Strong> <span class="text-danger">${element.todayDeaths}</span>
+               </div>`
             }
 
          
