1101926942868113136d2cdaa4abbc6534004e31
Diego De la Vega <diego_dv7@outlook.com>
2020-03-24 20:13:23 -0600

    fetch4

--- Fetch4MarkersApi/index.js
+++ Fetch4MarkersApi/index.js
@@ -20,12 +20,24 @@ function mapStart()
         console.log(data[0]);
         data.forEach(element => {
 
-            var info = `<img src="${element.countryInfo.flag}">`
+            var icon = {
+                url : "../Icons/corona.png",
+                scaledSize: new google.maps.Size(20, 20),
+                origin:  new google.maps.Point(0, 0),
+                anchor : new google.maps.Point(0, 0)
+            };
+
+            var info = `<img src="${element.countryInfo.flag}">
+            <br><Strong>Cases:</Strong> ${element.cases} 
+               <br><Strong>Deaths:</Strong> <span class="text-danger">${element.deaths}</span>
+               <br><Strong>Today cases:</Strong> ${element.todayCases}
+               <br><Strong>Today deaths:</Strong> <span class="text-danger">${element.todayDeaths}</span>`
 
             let marker = new google.maps.Marker({
                 map: map,
                 position: new google.maps.LatLng(element.countryInfo.lat, element.countryInfo.long),
-                title: element.country
+                title: element.country,
+                icon : icon
             });
 
             if(element.country.toLowerCase() == "usa" )
@@ -33,9 +45,9 @@ function mapStart()
                 marker.title = "United States";
                 marker.position =  new google.maps.LatLng(37.09024, -95.712891);
 
-               info = `<div class="text-center"><img class="responsiveImg"
+               info = `<img class="responsiveImg"
                src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png">
-               </div> <br><Strong>Cases:</Strong> ${element.cases} 
+               <br><Strong>Cases:</Strong> ${element.cases} 
                <br><Strong>Deaths:</Strong> <span class="text-danger">${element.deaths}</span>
                <br><Strong>Today cases:</Strong> ${element.todayCases}
                <br><Strong>Today deaths:</Strong> <span class="text-danger">${element.todayDeaths}</span>`
