18819c41d82541ef6c10f60174d5823ae87c7a59
Diego De la Vega <diego_dv7@outlook.com>
2020-03-24 19:00:22 -0600

    fetch4

--- Fetch4MarkersApi/index.js
+++ Fetch4MarkersApi/index.js
@@ -11,11 +11,21 @@ var properties = {
 
 function mapStart()
 {
+    const map = new google.maps.Map(document.getElementById("map"), properties);
    fetch("https://corona.lmao.ninja/countries")
    .then(function(response) {
     response.json().then(function(data){
-        console.log(data);
-        console.log('hola');
+
+
+        data.forEach(element => {
+                let marker = new google.maps.Marker({
+                    map: map,
+                    position: new google.maps.LatLng(element.lat, element.long),
+                    title: element.country
+                });
+           
+        });
+       
     })
 
    })
