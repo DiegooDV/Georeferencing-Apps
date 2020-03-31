445da410f419212d23abe83244ad82426f3772a1
Diego De la Vega <diego_dv7@outlook.com>
2020-03-31 17:22:29 -0600

    app 8

--- MapLang/index.js
+++ MapLang/index.js
@@ -1,23 +1,24 @@
 
 
 const coordinates = {
-    lat: -31.563910, 
-    lng: 147.154312
+    lat: 0, 
+    lng: 0
 }
 
 const queryString = window.location.search;
 
 const urlParams = new URLSearchParams(queryString);
 
-const idioma = urlParams.get('idiom');
+const idiom = urlParams.get('idiom');
 
 
-document.getElementById('idiom').value = idioma;
+document.getElementById('sltIdiom').value = idiom;
 
 
 var script = document.createElement('script');
-script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyADjOfdGIg-7JiqjoTstMN9el4g-nLhxxA&callback=mapStart&language=' + idioma ;
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCNe-mjWzhEUSwDH7BvgJWtMxZUZmmFI2U&callback=mapStart&language=' + idiom;
 document.head.appendChild(script);
+
 function mapStart() {
 
     var map = new google.maps.Map(
