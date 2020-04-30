e692639b19568bcff6fede68012e89aebf5e3c21
Diego De la Vega <diego_dv7@outlook.com>
2020-04-29 20:49:11 -0500

    firebase oauth

---
+++ FirebaseOauth2/scripts/authorization.js
@@ -0,0 +1,132 @@
+const userLoggedWithGoogle = false;
+auth.onAuthStateChanged(user => {
+
+    if(user)
+    {
+        db.collection("Menu").onSnapshot(snapshot => {
+         getMenu(snapshot.docs);
+        });
+        configureNavbar(user);
+    }
+    else{
+        getMenu(null);
+        configureNavbar(user);
+    }
+}); 
+
+const formLogin = document.getElementById("formLogin");
+formLogin.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let email = formLogin["email"].value;
+  let password = formLogin["password"].value;
+
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((credentials) => {
+      console.log(credentials);
+      $("#modalLogin").modal("hide");
+      formLogin.reset();
+      Swal.fire("Welcome home");
+    })
+    .catch((err) => {
+      Swal.fire("Error", err.message, "error");
+    });
+});
+
+const salir = document.getElementById("signOut");
+salir.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  Swal.fire({
+    title: "Sign out?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes",
+  }).then((result) => {
+    if (result.value) {
+      auth.signOut().then(() => {
+        userLoggedWithGoogle = false;
+        let timerInterval;
+        Swal.fire({
+          title: "Signing out",
+          html: "<b></b>",
+          timer: 1000,
+          timerProgressBar: true,
+          onBeforeOpen: () => {
+            Swal.showLoading();
+            timerInterval = setInterval(() => {
+              const content = Swal.getContent();
+              if (content) {
+                const b = content.querySelector("b");
+                if (b) {
+                  b.textContent = Swal.getTimerLeft();
+                }
+              }
+            }, 100);
+          },
+          onClose: () => {
+            clearInterval(timerInterval);
+          },
+        }).then((result) => {
+          if (result.dismiss === Swal.DismissReason.timer) {
+            Swal.fire("Signed out", "", "success");
+          }
+        });
+      });
+    }
+  });
+});
+
+const formRegister = document.getElementById("formRegister");
+
+formRegister.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    let email = formRegister["emailRegister"].value;
+    let password = formRegister["passwordRegister"].value;
+    let name = formRegister["nameRegister"].value;
+    let address = formRegister["addressRegister"].value;
+    let phone = formRegister["phoneRegister"].value;
+
+    auth.createUserWithEmailAndPassword(email, password).then( (credentials) => {
+        
+        return db.collection("Users").doc(credentials.user.uid).set({
+            name: name,
+            phone: phone,
+            address: address
+        });
+
+    }).then(() =>{
+        $("#modalRegister").modal("hide");
+        formRegister.reset();
+        Swal.fire("Welcome aboard");
+    }).catch((err) => {
+        Swal.fire("Error", err.message, "error");
+    });
+});
+
+googleLogin() = () => {
+    userLoggedWithGoogle = true;
+    let provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function(result){
+
+        var token = result.credential.accessToken;
+
+        var user = result.user;
+
+        let html =  `
+        <p>Name: ${user.displayName}</p>
+        <p>Email: ${user.displayEmail}</p> 
+        <img src='${user.phoroURL}'  style="width:40px; height: 40px;`;
+
+        $("#modalLogin").modal("hide");
+        formLogin.reset();
+        Swal.fire("Welcome home");
+    }).catch((err) => {
+        Swal.fire("Error", err.message, "error");
+      });
+    
+}
---
+++ FirebaseOauth2/scripts/index.js
@@ -0,0 +1,69 @@
+const accountInfo = document.querySelector(".accountInfo");
+const loggedInElements = document.querySelectorAll(".logged-in");
+const loggedOutElements = document.querySelectorAll(".logged-out");
+
+const configureNavbar = (user) => {
+  if (user) {
+    loggedInElements.forEach((item) => {
+      item.style.display = "block";
+    });
+    loggedOutElements.forEach((item) => {
+      item.style.display = "none";
+    });
+if(userLoggedWithGoogle)
+{
+
+    db.collection("Users").doc(user.uid).get().then( doc => {
+        const html = 
+        `<p>Name: ${doc.data().name}</p>
+        <p>Email: ${user.email}</p>
+        <p>Address: ${doc.data().address}</p>
+        <p>Phone: ${doc.data().phone}</p>`;
+
+        accountInfo.innerHTML = html;
+    });
+}
+
+  } else {
+    loggedInElements.forEach((item) => {
+      item.style.display = "none";
+    });
+    loggedOutElements.forEach((item) => {
+      item.style.display = "block";
+    });
+  }
+};
+
+const menu = document.getElementById("menu");
+
+const getMenu = (data) => {
+
+    let html = "";
+    if(data)
+    {
+        if(data.length >=1 )
+        {
+        data.forEach(doc => {
+            const column =  `<div class="col-12 col-md-4">
+            <img class="img-fluid w-100" src="img/${doc.data().image}" />
+            <h4>${doc.data().name}</h4>
+            <h4>$ ${doc.data().price}</h4>
+            <a href="https://paypal.com" target="_blank"><button class="btn btn-primary">Buy</button></a>
+          </div>`    
+
+          html += column;  
+        });
+
+        menu.innerHTML = html;
+        }
+        else{
+            html = "<h3 class='mt-5'>No pizzas available, try again later</h3>"
+            menu.innerHTML = html;
+        }
+      
+    }
+    else{
+        html = "<h3 class='mt-5'>You need to log in to see our menu</h3>"
+        menu.innerHTML = html;
+    }
+}
