5a40caf94d47796228c8fa9f07991497f060aa05
Diego De la Vega <diego_dv7@outlook.com>
2020-05-07 19:10:57 -0500

    app 9

---
+++ MapStyle/index.js
@@ -0,0 +1,210 @@
+
+        function mapStart(){
+            var styledMapType = new google.maps.StyledMapType(
+                [
+                    {
+                      "elementType": "geometry",
+                      "stylers": [
+                        {
+                          "color": "#242f3e"
+                        }
+                      ]
+                    },
+                    {
+                      "elementType": "labels.text.fill",
+                      "stylers": [
+                        {
+                          "color": "#746855"
+                        }
+                      ]
+                    },
+                    {
+                      "elementType": "labels.text.stroke",
+                      "stylers": [
+                        {
+                          "color": "#242f3e"
+                        }
+                      ]
+                    },
+                    {
+                      "featureType": "administrative.locality",
+                      "elementType": "labels.text.fill",
+                      "stylers": [
+                        {
+                          "color": "#d59563"
+                        }
+                      ]
+                    },
+                    {
+                      "featureType": "poi",
+                      "elementType": "labels.text.fill",
+                      "stylers": [
+                        {
+                          "color": "#d59563"
+                        }
+                      ]
+                    },
+                    {
+                      "featureType": "poi.park",
+                      "elementType": "geometry",
+                      "stylers": [
+                        {
+                          "color": "#263c3f"
+                        }
+                      ]
+                    },
+                    {
+                      "featureType": "poi.park",
+                      "elementType": "geometry.fill",
+                      "stylers": [
+                        {
+                          "color": "#b8e6b7"
+                        },
+                        {
+                          "lightness": -5
+                        }
+                      ]
+                    },
+                    {
+                      "featureType": "poi.park",
+                      "elementType": "labels.text.fill",
+                      "stylers": [
+                        {
+                          "color": "#6b9a76"
+                        }
+                      ]
+                    },
+                    {
+                      "featureType": "poi.school",
+                      "elementType": "geometry",
+                      "stylers": [
+                        {
+                          "color": "#b7dae6"
+                        }
+                      ]
+                    },
+                    {
+                      "featureType": "road",
+                      "elementType": "geometry",
+                      "stylers": [
+                        {
+                          "color": "#38414e"
+                        }
+                      ]
+                    },
+                    {
+                      "featureType": "road",
+                      "elementType": "geometry.stroke",
+                      "stylers": [
+                        {
+                          "color": "#212a37"
+                        }
+                      ]
+                    },
+                    {
+                      "featureType": "road",
+                      "elementType": "labels.text.fill",
+                      "stylers": [
+                        {
+                          "color": "#9ca5b3"
+                        }
+                      ]
+                    },
+                    {
+                      "featureType": "road.highway",
+                      "elementType": "geometry",
+                      "stylers": [
+                        {
+                          "color": "#746855"
+                        }
+                      ]
+                    },
+                    {
+                      "featureType": "road.highway",
+                      "elementType": "geometry.stroke",
+                      "stylers": [
+                        {
+                          "color": "#1f2835"
+                        }
+                      ]
+                    },
+                    {
+                      "featureType": "road.highway",
+                      "elementType": "labels.text.fill",
+                      "stylers": [
+                        {
+                          "color": "#f3d19c"
+                        }
+                      ]
+                    },
+                    {
+                      "featureType": "transit",
+                      "elementType": "geometry",
+                      "stylers": [
+                        {
+                          "color": "#2f3948"
+                        }
+                      ]
+                    },
+                    {
+                      "featureType": "transit.station",
+                      "elementType": "labels.text.fill",
+                      "stylers": [
+                        {
+                          "color": "#d59563"
+                        }
+                      ]
+                    },
+                    {
+                      "featureType": "water",
+                      "elementType": "geometry",
+                      "stylers": [
+                        {
+                          "color": "#17263c"
+                        }
+                      ]
+                    },
+                    {
+                      "featureType": "water",
+                      "elementType": "labels.text.fill",
+                      "stylers": [
+                        {
+                          "color": "#515c6d"
+                        }
+                      ]
+                    },
+                    {
+                      "featureType": "water",
+                      "elementType": "labels.text.stroke",
+                      "stylers": [
+                        {
+                          "color": "#17263c"
+                        }
+                      ]
+                    }
+                  ],
+                        {name: 'Dark Map'}
+            );
+
+            var propiedades = {
+                center: { 
+                     lat: 21.152639, lng:  -101.711598
+                },
+                zoom: 14,
+                mapTypeControlOptions: {
+                    mapTypeIds : [ 'roadmap', 'satellite', 'hybrid', 'terrain', 'style_map']
+                }
+                
+            };
+
+            var mapa = document.getElementById("map");
+
+            var map = new google.maps.Map(mapa, propiedades);
+
+            map.mapTypes.set('style_map', styledMapType);
+            map.setMapTypeId('style_map');
+
+
+
+        }
+
