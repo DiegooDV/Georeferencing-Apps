8ced297409bb421684b52c9a4cfd536329ba0a67
Diego De la Vega <diego_dv7@outlook.com>
2020-06-14 15:54:57 -0500

    commit

--- Chatter/js/index.js
+++ Chatter/js/index.js
@@ -255,9 +255,11 @@ async function loadMessages(snapshot)
   await db.collection("Messages").get().then((messages) => {
     console.log(messages);
     friends.forEach(friend => {
-      let chat = messages.doc.data().filter(function (el) {
+      let chat = messages.docs.filter(function (el) {
         return (el.from == friend || el.from == userD.uid) && (el.to == friend || el.to == userD.uid)
       });
+
+      console.log(chat);
 
       let chatHtml = document.getElementById(`chat${friend}`);
       let html = "";
